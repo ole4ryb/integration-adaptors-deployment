de3b8a18597c1eb4c485335d9ff3c920083b2f8b
Adrian Clay <adrian.clay1@nhs.net>
2024-08-30 16:58:20 +0100

    Update GP Connect Consumer Adaptor to latest version (#2)

--- aws/components/gp2gp/gpc-consumer-variables.tf
+++ aws/components/gp2gp/gpc-consumer-variables.tf
@@ -32,7 +32,7 @@ variable "gpc-consumer_service_launch_type" {
 variable "gpc-consumer_build_id" {
   type = string
   description = "Number of the current build, used for tagging the logs"
-  default = "main-113-45147d5"
+  default = "main-124-e1a5b55"
 }
 
 variable "gpc-consumer_environment_variables" {
